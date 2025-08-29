9ef7bda997267e047296462248e13feb93af1dd8
Djiamil <djiamil02@outlook.com>
2025-08-29 12:25:39 +0200

    config deployement

--- gestion_catastrophes/settings.py
+++ gestion_catastrophes/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
-import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -112,19 +111,11 @@ AUTH_USER_MODEL = 'api.User'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'gestion_catastrophes_db',
-#         'USER': 'gestion_catastrophe_user',
-#         'PASSWORD': 'gestion_catastrophe_2024!',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
-    "default": dj_database_url.config(default=os.getenv("DATABASE_URL") or os.getenv("URL_BASE_DE_DONNÉES"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 }
 
 
