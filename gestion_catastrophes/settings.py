f3d826dde229476708d95528c680a0b33af81ff4
Djiamil <djiamil02@outlook.com>
2025-08-29 13:44:46 +0200

    Add Render domain to ALLOWED_HOSTS

--- gestion_catastrophes/settings.py
+++ gestion_catastrophes/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-@s(4jv8xwh%nzb+mn$6n5&smh=hl8a0v&pnjq32h0r_i_xnq=4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'gestion-catastrophe.onrender.com']
 
 
 # Application definition
